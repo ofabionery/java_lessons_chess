61594c4730200cb844e1786c9f533a26408dfc0c
ofabionery <ofabionery@gmail.com>
2021-02-17 17:36:25 -0300

    Starting to implement Board and Piece

--- src/java_lessons_chess/application/Program.java
+++ src/java_lessons_chess/application/Program.java
@@ -1,13 +1,13 @@
 package java_lessons_chess.application;
 
-import java_lessons_chess.boardgame.Position;
+import java_lessons_chess.boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8, 8);
+		
 
 	}
 
